f59356ea2fc77c023c09d8278efab59a4355d3c4
Taposh paul <taposhp05@gmail.com>
2024-11-18 22:43:24 +0600

    resopsive menu bar and header section

---
+++ main.js
@@ -0,0 +1,7 @@
+const barsIcon = document.getElementById("bars");
+const menuBar = document.querySelector(".menu-ber");
+
+barsIcon.addEventListener("click", () => {
+    menuBar.classList.toggle("active");
+
+});
