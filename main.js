c945b9ee0d0fad1b520baaa30e8f80f762440191
Taposh paul <taposhp05@gmail.com>
2024-11-23 00:02:04 +0600

    create scroll up botton

--- main.js
+++ main.js
@@ -1,7 +1,31 @@
-const barsIcon = document.getElementById("bars");
-const menuBar = document.querySelector(".menu-ber");
 
-barsIcon.addEventListener("click", () => {
-    menuBar.classList.toggle("active");
+// menu bar 
+const mobileMenu = function () {
+    const barsIcon = document.getElementById("bars");
+    const menuBar = document.querySelector(".menu-ber");
 
-});
+    barsIcon.addEventListener("click", () => {
+        menuBar.classList.toggle("active");
+
+    });
+}
+mobileMenu();
+
+// scroll-up btn 
+const srcollUp = function () {
+    const scrollUpBtn = document.getElementById("scrollUp");
+
+    // scroll upBtn hide and show 
+    const ScrollBtn = function () {
+        window.scrollY > window.innerHeight ? scrollUpBtn.classList.add("show") : scrollUpBtn.classList.remove("show");
+    }
+    window.addEventListener("scroll", ScrollBtn);
+    // scroll up function
+    scrollUpBtn.addEventListener("click", () => {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        })
+    })
+};
+srcollUp();
